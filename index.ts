1f57a6b1cb550c2cde8aea9158a9dbd1fcb1f9a8
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2023-04-25 08:50:34 -0600

    make interactive

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 //@author  @bakedpotatolord
+import ReadLine from 'readline/promises';
+import chalk from 'chalk';
 
 let numWhite = 50
 let numBlack = 50
@@ -16,45 +18,81 @@ interface generationData{
   whiteFrequency?:number
 }
 
+
 let data :generationData[] = [createData()]
 
-while(numWhite >0){
-  gen++
-  
-  tempWhite = 0
-  tempBlack = 0
+const readLine = ReadLine.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
 
-  while(numWhite > 0 || numBlack > 0){
-    
-    let t1 = pickRandom(numWhite,numBlack)
-    let t2 =  t1? pickRandom(numWhite,numBlack-1):pickRandom(numWhite-1,numBlack) 
+console.log(chalk.greenBright('welcome to the natural selection simulation'))
+getOption()
 
-    if(t1 && t2){
-      numBlack-=2
-      tempBlack+=2
-    }else if((t1 || t2)){
-      //remove from bag
-      numWhite--
-      numBlack--
-      //add to outside bag
-      tempWhite++
-      tempBlack++
-    }else{
-      //remove 2 from outside bag 
-      numWhite-=2
-    }
+
+async function getOption(){
+  console.log(" 'W' to run simuation, 'Q' to quit, 'S' to do shit")
+  switch(await readLine.question('what do you want to do? ')){
+    case 'W':
+      runSimulation()
+      getOption()
+      break
+    case 'Q':
+      process.exit(0)
+    case 'S':
+      console.log(chalk.red('shit is done'))
+      getOption()
+      break
   }
-  //put remains back in bag
-  numWhite = tempWhite
-  numBlack = tempBlack
-
-  data.push(createData())
-
 }
-console.log("\nsimulation complete")
-console.table(data)
 
-function pickRandom( white : number,black:number){
+function reset(){
+  numWhite = 50
+  numBlack = 50
+  gen = 0
+  data = [createData()]
+}
+
+function runSimulation(){
+  reset()
+  while(numWhite >0){
+    gen++
+    
+    tempWhite = 0
+    tempBlack = 0
+  
+    while(numWhite > 0 || numBlack > 0){
+      
+      let t1 = pickRandom(numWhite,numBlack)
+      let t2 =  t1? pickRandom(numWhite,numBlack-1):pickRandom(numWhite-1,numBlack) 
+  
+      if(t1 && t2){
+        numBlack-=2
+        tempBlack+=2
+      }else if((t1 || t2)){
+        //remove from bag
+        numWhite--
+        numBlack--
+        //add to outside bag
+        tempWhite++
+        tempBlack++
+      }else{
+        //remove 2 from outside bag 
+        numWhite-=2
+      }
+    }
+    //put remains back in bag
+    numWhite = tempWhite
+    numBlack = tempBlack
+  
+    data.push(createData())
+  
+  }
+  console.log("\nsimulation complete")
+  console.table(data)
+}
+
+export function pickRandom( white : number,black:number){
   // true-> black
   // false-> white
   return Math.random() < (black/(black + white))
